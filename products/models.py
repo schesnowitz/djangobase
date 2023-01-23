ad9d332b3c34d1e81f2e1d6b4d9b2fd0cb3f71e3
Stephen Chesnowitz <steve@chesnowitz.com>
2023-01-23 02:16:13 -0500

    update model field types

--- products/models.py
+++ products/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Product(models.Model):
-    title       = models.TextField()
+    title       = models.CharField(max_length=120)
     description = models.TextField()
-    price       = models.TextField()
-    summary       = models.TextField(default='some text default in summary')
+    price       = models.DecimalField(decimal_places=2, max_digits=10000)
+    summary     = models.TextField()
